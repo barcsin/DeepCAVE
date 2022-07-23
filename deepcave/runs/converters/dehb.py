57f895dd3e0230636036ef88fb398f9fb4a09120
Aron Bahram <aron.bahram@gmail.com>
2022-07-23 17:20:00 +0200

    Update DEHB converter

--- deepcave/runs/converters/dehb.py
+++ deepcave/runs/converters/dehb.py
@@ -1,17 +1,19 @@
 """
 This package defines converters for the outputs of some optimizers, that then can be interpreted by DeepCAVE.
 """
-import os
 import pickle
 from pathlib import Path
+
+import ConfigSpace
+import ConfigSpace.util
+import numpy as np
+
 from deepcave.runs.run import Run
 from deepcave import Objective
-from deepcave.utils.configs import op_indices2config
 from deepcave.utils.hash import file_to_hash
 
 
 class DEHBRun(Run):
-
     prefix = 'dehb'
     _initial_order = 1
 
@@ -24,9 +26,8 @@ class DEHBRun(Run):
         """
         if self.path is None:
             return ""
-        # Find the name of the history file under the path
-        file_name = str([x for x in os.listdir(self.path) if str(x).startswith("hist") and str(x).endswith(".pkl")][0])
-        return file_to_hash(self.path / file_name)
+
+        return file_to_hash(self.path / "history_dehb.pkl")
 
     @property
     def latest_change(self):
@@ -37,9 +38,9 @@ class DEHBRun(Run):
         """
         if self.path is None:
             return 0
-        # Find the name of the history file under the path
-        file_name = str([x for x in os.listdir(self.path) if str(x).startswith("hist") and str(x).endswith(".pkl")][0])
-        return Path(self.path / file_name).stat().st_mtime
+        # You can change the file name to the one that your DEHB run generates (this can be controlled with the name
+        # parameter in DEHB's run method)
+        return Path(self.path / "history_dehb.pkl").stat().st_mtime
 
     @classmethod
     def from_path(cls, path):
@@ -51,23 +52,31 @@ class DEHBRun(Run):
         """
         path = Path(path)
 
-        # Find the name of the history file under the path
-        file_name = str([x for x in os.listdir(path) if str(x).startswith("hist") and str(x).endswith(".pkl")][0])
-
         # Read the configspace of the search space
+        # DEHB does not have a configspace.json by default, so generate one yourself in the output folder
+        # This might change in the future
         from ConfigSpace.read_and_write import json as cs_json
         with (path / "configspace.json").open("r") as f:
             configspace = cs_json.read(f.read())
 
         # Read history, which stores all the relevant data for a single optimization run
-        with open(path / file_name, "rb") as f:
+        # You can change the file name to the one that your DEHB run generates (this can be controlled with the name
+        # parameter in DEHB's run method)
+        with open(path / "history_dehb.pkl", "rb") as f:
             history = pickle.load(f)
 
         # Define objective of the optimization, this is needed for DeepCAVE
-        objective = Objective("regret", lower=0, upper=100)
+        obj1 = Objective("Train loss", lower=0, upper=100)
+        obj2 = Objective("Validation loss", lower=0, upper=100)
+        obj3 = Objective("Test loss", lower=0, upper=100)
+        obj4 = Objective("Train regret", lower=0, upper=100)
+        obj5 = Objective("Validation regret", lower=0, upper=100)
+        obj6 = Objective("Test regret", lower=0, upper=100)
+        obj7 = Objective("Train time", lower=0)
+        objectives = [obj1, obj2, obj3, obj4, obj5, obj6, obj7]
 
         # Create the run, which will store all the optimization steps a.k.a. trials
-        run = DEHBRun(path.stem, configspace=configspace, objectives=objective, meta={})
+        run = DEHBRun(path.stem, configspace=configspace, objectives=objectives, meta={})
         # Remember to set the path of the Run manually
         run._path = path
 
@@ -77,25 +86,74 @@ class DEHBRun(Run):
         # Let's loop through the history of the optimization run to extract this information and add it one-by-one
         # to the run object we just defined
         for result in history:
-            # Because the DEHB representation of configuration is a list of continues values, we will use the
-            # NAS-Bench-201 representation instead, which is a discrete version of it, called operation indices
-            config_dehb = result[0]
-            regret = result[1]
+            # DEHB represents a configuration as a list of continuous values in the range (0, 1), called vector
+            dehb_vector = result[0]
+            valid_regret = result[1]
             train_time = result[2]
             budget = int(result[3])
             info = result[4]
-            # Get the operation indices and convert them to configspace objects
-            config = op_indices2config(config_dehb)
-            # simulate train time
+            train_loss = info['train_loss']
+            valid_loss = info['valid_loss']
+            test_loss = info['test_loss']
+            train_regret = info['train_regret']
+            test_regret = info['test_regret']
+
+            config = cls.vector_to_configspace(dehb_vector, configspace)
+            # Simulate train time
             end_time = start_time + train_time
 
-            run.add(costs=regret,
+            run.add(costs=[train_loss,
+                           valid_loss,
+                           test_loss,
+                           train_regret,
+                           valid_regret,
+                           test_regret,
+                           train_time],
                     config=config,
                     budget=budget,
                     start_time=start_time,
-                    end_time=end_time,
-                    additional=info)
+                    end_time=end_time)
 
             start_time = end_time
         return run
 
+    @classmethod
+    def vector_to_configspace(cls, vector: np.array, cs: ConfigSpace) -> ConfigSpace.Configuration:
+        """Converts numpy array to ConfigSpace object
+
+        Works when cs is a ConfigSpace object and the input vector is in the domain [0, 1].
+
+        Notes:
+            Code copied from the DEHB project
+            https://github.com/automl/DEHB/blob/master/dehb/optimizers/de.py
+        """
+        # creates a ConfigSpace object dict with all hyperparameters present, the inactive too
+        new_config = ConfigSpace.util.impute_inactive_values(
+            cs.sample_configuration()
+        ).get_dictionary()
+        # iterates over all hyperparameters and normalizes each based on its type
+        for i, hyper in enumerate(cs.get_hyperparameters()):
+            if type(hyper) == ConfigSpace.OrdinalHyperparameter:
+                ranges = np.arange(start=0, stop=1, step=1 / len(hyper.sequence))
+                param_value = hyper.sequence[np.where((vector[i] < ranges) == False)[0][-1]]
+            elif type(hyper) == ConfigSpace.CategoricalHyperparameter:
+                ranges = np.arange(start=0, stop=1, step=1 / len(hyper.choices))
+                param_value = hyper.choices[np.where((vector[i] < ranges) == False)[0][-1]]
+            else:  # handles UniformFloatHyperparameter & UniformIntegerHyperparameter
+                # rescaling continuous values
+                if hyper.log:
+                    log_range = np.log(hyper.upper) - np.log(hyper.lower)
+                    param_value = np.exp(np.log(hyper.lower) + vector[i] * log_range)
+                else:
+                    param_value = hyper.lower + (hyper.upper - hyper.lower) * vector[i]
+                if type(hyper) == ConfigSpace.UniformIntegerHyperparameter:
+                    param_value = int(np.round(param_value))  # converting to discrete (int)
+                else:
+                    param_value = float(param_value)
+            new_config[hyper.name] = param_value
+        # the mapping from unit hypercube to the actual config space may lead to illegal
+        # configurations based on conditions defined, which need to be deactivated/removed
+        new_config = ConfigSpace.util.deactivate_inactive_hyperparameters(
+            configuration=new_config, configuration_space=cs
+        )
+        return new_config
